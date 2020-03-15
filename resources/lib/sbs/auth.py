05e8b6942263b40e7d6a22a255e8531f9fa6ae19
Michaël Arnauts <michael.arnauts@gmail.com>
2020-03-15 15:58:27 +0100

    Improve auth by not depending on boto3 and warrant

--- resources/lib/sbs/auth.py
+++ resources/lib/sbs/auth.py
@@ -3,33 +3,83 @@
 
 from __future__ import absolute_import, division, unicode_literals
 
+import json
+import logging
+from datetime import datetime
+
+from resources.lib.sbs.auth_awsidp import AwsIdp
+
+_LOGGER = logging.getLogger('auth')
+
 
 class SbsAuth:
     COGNITO_REGION = 'eu-west-1'
     COGNITO_POOL_ID = 'eu-west-1_dViSsKM5Y'
     COGNITO_CLIENT_ID = '6s1h851s8uplco5h6mqh1jac8m'
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, cache=None):
         """ Initialise object """
         self._username = username
         self._password = password
+        self._cache = cache
+        self._id_token = None
+        self._expiry = 0
+        self._refresh_token = None
+
+        if self._cache:
+            # Load tokens from cache
+            try:
+                with open(self._cache, 'r') as f:
+                    data_json = json.loads(f.read())
+                    self._id_token = data_json.get('id_token')
+                    self._refresh_token = data_json.get('refresh_token')
+                    self._expiry = int(data_json.get('expiry', 0))
+            except (FileNotFoundError, TypeError):
+                _LOGGER.info('We could not use the cache since it is invalid or non-existant.')
 
     def get_token(self):
-        """ Returns an authentication token """
-        import boto3
-        from warrant.aws_srp import AWSSRP
+        """ Get a valid token """
+        now = int(datetime.today().timestamp())
 
-        client = boto3.client('cognito-idp',
-                              region_name=self.COGNITO_REGION,
-                              aws_access_key_id='',
-                              aws_secret_access_key='')
+        if self._id_token and self._expiry > now:
+            # We have a valid id token in memory, use it
+            _LOGGER.debug('Got an id token from memory: %s', self._id_token)
+            return self._id_token
 
-        aws = AWSSRP(username=self._username,
-                     password=self._password,
-                     pool_id=self.COGNITO_POOL_ID,
-                     client_id=self.COGNITO_CLIENT_ID,
-                     client=client)
+        if self._refresh_token:
+            # We have a valid refresh token, use that to refresh our id token
+            id_token = self._refresh(self._refresh_token)
+            self._id_token = id_token
+            self._expiry = now + 3600
+            _LOGGER.debug('Got an id token by refreshing: %s', self._id_token)
 
-        result = aws.authenticate_user()
+        else:
+            # We have not tokens, or they are all invalid, do a login
+            id_token, refresh_token = self._authenticate(self._username, self._password)
+            self._id_token = id_token
+            self._refresh_token = refresh_token
+            self._expiry = now + 3600
+            _LOGGER.debug('Got an id token by logging in: %s', self._id_token)
 
-        return result.get('AuthenticationResult', {}).get('IdToken')
+        if self._cache:
+            # Store new tokens in cache
+            with open(self._cache, 'w') as f:
+                f.write(json.dumps(dict(
+                    id_token=self._id_token,
+                    refresh_token=self._refresh_token,
+                    expiry=self._expiry,
+                )))
+
+        return self._id_token
+
+    @staticmethod
+    def _authenticate(username, password):
+        """ Authenticate with Amazon Cognito and fetch a refresh token and id token. """
+        client = AwsIdp(SbsAuth.COGNITO_POOL_ID, SbsAuth.COGNITO_CLIENT_ID)
+        return client.authenticate(username, password)
+
+    @staticmethod
+    def _refresh(refresh_token):
+        """ Use the refresh token to fetch a new id token. """
+        client = AwsIdp(SbsAuth.COGNITO_POOL_ID, SbsAuth.COGNITO_CLIENT_ID)
+        return client.renew_token(refresh_token)
--- test/test_auth.py
+++ test/test_auth.py
@@ -18,17 +18,19 @@ class TestAuth(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestAuth, self).__init__(*args, **kwargs)
 
-    def test_login_ok(self):
+    def test_login(self):
         # TODO: use a credentials.json or something
-        auth = SbsAuth(os.getenv('VVZ_USERNAME', ''), os.getenv('VVZ_PASSWORD', ''))
+        auth = SbsAuth(os.getenv('VVZ_USERNAME', ''), os.getenv('VVZ_PASSWORD', ''), cache='/tmp/viervijfzes-tokens.json')
+
+        # We should get a token by logging in or refreshing
         token = auth.get_token()
         self.assertTrue(token)
         self.assertIsInstance(token, str)
 
-    def test_login_nok(self):
-        auth = SbsAuth('unknown@example.com', 'password')
-        with self.assertRaises(Exception):
-            auth.get_token()
+        # Test it a second time, it should go from memory now
+        token = auth.get_token()
+        self.assertTrue(token)
+        self.assertIsInstance(token, str)
 
 
 if __name__ == '__main__':
