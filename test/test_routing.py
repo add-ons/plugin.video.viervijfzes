55c8d1b16e78d13b303b1514ff6b2fe95ff5fc76
Michaël Arnauts <michael.arnauts@gmail.com>
2020-03-23 16:24:27 +0100

    Remove comment

--- test/test_routing.py
+++ test/test_routing.py
@@ -58,7 +58,6 @@ class TestRouting(unittest.TestCase):
 
     def test_metadata_update(self):
         routing.run([routing.url_for(addon.metadata_clean), '0', ''])
-        # routing.run([routing.url_for(addon.metadata_update), '0', ''])
 
 
 if __name__ == '__main__':
