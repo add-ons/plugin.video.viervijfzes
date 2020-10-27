49730594c51abb2b7d5544fe8a3d2f8a7dcf381f
Michaël Arnauts <michael.arnauts@gmail.com>
2020-10-27 08:58:34 +0100

    Remove obsolete line

--- tests/__init__.py
+++ tests/__init__.py
@@ -27,7 +27,6 @@ if sys.version_info[0] == 2:
 # Set credentials based on environment data
 # Use the .env file with Pipenv to make this work nicely during development
 ADDON = xbmcaddon.Addon()
-ADDON.setSetting('loginprovider', '0')  # Streamz login
 if os.environ.get('ADDON_USERNAME'):
     ADDON.setSetting('username', os.environ.get('ADDON_USERNAME'))
 if os.environ.get('ADDON_PASSWORD'):
