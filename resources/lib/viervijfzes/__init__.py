4af8d66d362863af9052cf1ba883ac7a5efc0628
mediaminister <mediaminister@gmail.com>
2023-03-06 19:02:55 +0100

    Add DRM support for all streams

--- resources/lib/viervijfzes/__init__.py
+++ resources/lib/viervijfzes/__init__.py
@@ -79,19 +79,17 @@ STREAM_DICT = {
 class ResolvedStream:
     """ Defines a stream that we can play"""
 
-    def __init__(self, uuid=None, url=None, stream_type=None, license_url=None, auth=None):
+    def __init__(self, uuid=None, url=None, stream_type=None, license_key=None):
         """
         :type uuid: str
         :type url: str
         :type stream_type: str
-        :type license_url: str
-        :type auth: str
+        :type license_key: str
         """
         self.uuid = uuid
         self.url = url
         self.stream_type = stream_type
-        self.license_url = license_url
-        self.auth = auth
+        self.license_key = license_key
 
     def __repr__(self):
         return "%r" % self.__dict__
